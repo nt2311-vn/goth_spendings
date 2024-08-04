d09c3095bad813ebd0aedd6bb0d0b5f72a945808
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-04 23:35:42 +0700

    main entry of code

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+	"github.com/nt2311-vn/goth_spendings/handlers"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+	r := chi.NewRouter()
+	r.Get("/greet", handlers.Make(handlers.HandleGreet))
+
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	slog.Info("HTTP server started", "listenAddr", listenAddr)
+
+	http.ListenAndServe(listenAddr, r)
 }
