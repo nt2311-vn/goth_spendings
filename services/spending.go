8177c12565443094cc7806cf78f137b30bed9981
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-09 00:45:44 +0700

    service spendings to interact with spendings in db

--- services/spending.go
+++ services/spending.go
@@ -31,3 +31,15 @@ func (s *SpendingService) UpdateItem(id string, newVal db.Spending) error {
 func (s *SpendingService) DeleteItem(id string) error {
 	return s.store.Delete(id)
 }
+
+type BalanceService struct {
+	store db.BalanceStore
+}
+
+func NewBalanceService(store db.BalanceStore) *BalanceService {
+	return &BalanceService{store: store}
+}
+
+func (b *BalanceService) GetBalance() int64 {
+	return b.store.GetBalance()
+}
