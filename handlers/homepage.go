c3c2adf8ed315f8a3f508d22301c39c6def5c9d9
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-09 00:45:04 +0700

    implement spendings and balance in hompage handlers

--- handlers/homepage.go
+++ handlers/homepage.go
@@ -1,11 +1,32 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/nt2311-vn/goth_spendings/components"
+	"github.com/nt2311-vn/goth_spendings/db"
+	"github.com/nt2311-vn/goth_spendings/services"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) error {
-	return Render(w, r, components.Index())
+	balanceStore := db.NewBalanceStoreJson()
+	spendingStore := db.NewSpendingStoreJson()
+	balanceService := services.NewBalanceService(balanceStore)
+	spendingService := services.NewSpendingService(spendingStore)
+
+	spendings, err := spendingService.ListItems()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return fmt.Errorf("Cannot list item on spendings")
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+
+	return Render(
+
+		w,
+		r,
+		components.Index(balanceService.GetBalance(), spendings),
+	)
 }
