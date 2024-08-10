093add7d13f2e7cafde4e7ff27bafa5ca973305e
nt2311-vn <nguyentoan231196@gmail.com>
2024-08-10 10:17:19 +0700

    main.go hanlder

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
+	"github.com/nt2311-vn/goth_spendings/components"
 	"github.com/nt2311-vn/goth_spendings/db"
 	"github.com/nt2311-vn/goth_spendings/handlers"
 	"github.com/nt2311-vn/goth_spendings/services"
@@ -23,20 +26,29 @@ func main() {
 	balanceService := services.NewBalanceService(balanceStore)
 	spendingService := services.NewSpendingService(spendingStore)
 
-	h := &handlers.Handlers{
-		SpendingService: spendingService,
-		BalanceService:  balanceService,
-	}
-
 	spendingHandler := handlers.NewSpendingHandler(*spendingService)
 
 	r := chi.NewRouter()
-	r.Get("/", handlers.Make(h.HomePage))
-	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	r.Post("/spending/add", spendingHandler.HandleAddSpendingItem)
-	r.Put("/spending", spendingHandler.HandleUpdateSpendingItem)
-	r.Delete("/spending", spendingHandler.HandleRemoveSpendingItem)
+	// Define the main page route
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		spendings, err := spendingService.ListItems()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html")
+		components.Index(balanceService.GetBalance(), spendings).Render(r.Context(), w)
+	})
+
+	// Serve static files
+	fmt.Println("Static files path: ", filepath.Join("static"))
+	staticDir := filepath.Join("static")
+	r.Handle("/static/**", http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
+
+	r.Post("/api/spending", spendingHandler.HandleAddSpendingItem)
+	r.Put("/api/spending", spendingHandler.HandleUpdateSpendingItem)
+	r.Delete("/api/spending", spendingHandler.HandleRemoveSpendingItem)
 
 	listenAddr := os.Getenv("LISTEN_ADDR")
 	slog.Info("HTTP server started", "listenAddr", listenAddr)
